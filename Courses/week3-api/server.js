febdd33eeb32759c81a843d96b63386a0742ba71
Kwizera Emmanuel <51673007+Emmanuel-kwizera@users.noreply.github.com>
2026-04-23 10:46:52 +0200

    rf: Add POST and DELETE endpoints for announcements

--- Courses/week3-api/server.js
+++ Courses/week3-api/server.js
@@ -15,7 +15,6 @@ const dataPath = path.join(__dirname, 'data', 'announcements.json');
 // Helper functions to read and write the announcements data
 async function readAll() {
   const raw = await fs.readFile(dataPath, 'utf8');
-  console.log("readAll called");
   return JSON.parse(raw);
 }
 
@@ -31,7 +30,7 @@ app.get('/announcements', async (req, res) => {
     res.json({ count: announcements.length, announcements: announcements })
     // res.json(announcements);
   } catch (err) {
-    res.status(500).json({ error: 'Failed to read announcements' });
+    res.status(500).json({ error: err.message });
   }
 
 });
@@ -52,7 +51,38 @@ app.get("/announcements/:id", async (req, res) => {
   }
 });
 
+app.post('/announcements/add', async (req, res) => {
+  try {
+    const { title, message, category } = req.body;
+    const announcements = await readAll();
+    const newId = announcements.length > 0 ? Math.max(...announcements.map(ann => ann.id)) + 1 : 1;
+    const newAnnouncement = { id: newId, title, message, category };
 
+    announcements.push(newAnnouncement);
+    await writeAll(announcements);
+    res.status(201).json(newAnnouncement);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.delete('/announcements/delete/:id', async (req, res) => {
+  const id = Number(req.params.id);
+  try {
+    const announcements = await readAll();
+    const index = announcements.findIndex((ann) => ann.id === id);
+    if (index !== -1) {
+      const deletedAnnouncement = announcements.splice(index, 1)[0];
+      await writeAll(announcements);
+      res.json(deletedAnnouncement);
+    } else {
+      res.status(404).json({ error: 'Announcement not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });     
+  }
+});
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
